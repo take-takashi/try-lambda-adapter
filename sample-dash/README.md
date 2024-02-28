e40a9d0633a463112f3c66975d3f4d01bbd3e8d1
TAKE Takashi <705098+take-takashi@users.noreply.github.com>
2024-02-28 15:16:29 +0000

    マルチページ対応完了

--- sample-dash/README.md
+++ sample-dash/README.md
@@ -21,5 +21,16 @@ Using version ^2.2.1 for pandas
 # build
 (sample-dash-py3.11) @take-takashi ➜ .../try-lambda-adapter/sample-dash/src/sample_dash (main) $ docker build -f Dockerfile -t sample-dash .
 # debug
-(sample-dash-py3.11) @take-takashi ➜ .../try-lambda-adapter/sample-dash/src/sample_dash (main) $ docker run -p 8080:8080 sample-dash
+(sample-dash-py3.11) @take-takashi ➜ .../try-lambda-adapter/sample-dash/src/sample_dash (main) $ docker run -p 8080:8080 --rm sample-dash
 ```
+
+
+# TODO
+- [-] マルチページ対応
+- [ ] 実際のデプロイ
+
+# Ref
+- Dashをgunicornで起動するため  
+  https://github.com/yaojiach/docker-dash/blob/main/README.md
+- マルチページ対応  
+  https://dash.plotly.com/urls
--- sample-dash/src/sample_dash/app.py
+++ sample-dash/src/sample_dash/app.py
@@ -1,12 +1,24 @@
 from dash import Dash, html, dcc, callback, Output, Input
+import dash
 import plotly.express as px
 import pandas as pd
 
 df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/gapminder_unfiltered.csv')
 
-app = Dash(__name__)
+app = Dash(__name__, use_pages=True)
 server = app.server
 
+app.layout = html.Div([
+    html.H1('Multi-page app with Dash Pages'),
+    html.Div([
+        html.Div(
+            dcc.Link(f"{page['name']} - {page['path']}", href=page["relative_path"])
+        ) for page in dash.page_registry.values()
+    ]),
+    dash.page_container
+])
+
+"""
 app.layout = html.Div([
     html.H1(children='Title of Dash App', style={'textAlign':'center'}),
     dcc.Dropdown(df.country.unique(), 'Canada', id='dropdown-selection'),
@@ -20,6 +32,7 @@ app.layout = html.Div([
 def update_graph(value):
     dff = df[df.country==value]
     return px.line(dff, x='year', y='pop')
+"""
 
 if __name__ == '__main__':
     app.run(debug=True)
