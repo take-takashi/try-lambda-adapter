42278c0beb7620551eb4b36f0fc521d6e93659d0
TAKE Takashi <705098+take-takashi@users.noreply.github.com>
2024-02-28 14:29:00 +0000

    add settings

--- .devcontainer/postCreateCommand.bash
+++ .devcontainer/postCreateCommand.bash
@@ -5,7 +5,7 @@ WORKSPACE=$PWD
 # __pycache__フォルダを作成しない設定
 echo "export PYTHONDONTWRITEBYTECODE=1" >> ~/.bashrc
 
-# sample_langchainのセットアップ
-# cd ${WORKSPACE}/sample_langchain
-# poetry install --no-root
-# poetry shell &
+# sample_dashのセットアップ
+cd ${WORKSPACE}/sample_dash
+poetry install --no-root
+poetry shell &
